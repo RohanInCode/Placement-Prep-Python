d47d7dee16fcd0d16db8a767207a402477ce404e
RohanInCode <rohan.41167@gmail.com>
2026-01-30 09:09:58 +0530

    Day 3

---
+++ 05_Filehandling.py
@@ -0,0 +1,8 @@
+# =====================================================
+# 🔹 FILE HANDLING
+# =====================================================
+
+file = open("student.txt", "w")
+file.write("Name: Rohan\n")
+file.write("Course: Python\n")
+file.close()
