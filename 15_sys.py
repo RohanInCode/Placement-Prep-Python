372d172e941d6f4e7364537615b7a72521223b5d
RohanInCode <rohan.41167@gmail.com>
2026-02-02 00:41:19 +0530

    Day 9

---
+++ 15_sys.py
@@ -0,0 +1,10 @@
+# =====================================================
+# 7️⃣ SYS MODULE (System Info & Arguments)
+# =====================================================
+
+import sys
+
+print("\n--- SYS Demo ---")
+
+print("Platform:", sys.platform)
+print("Arguments:", sys.argv)
